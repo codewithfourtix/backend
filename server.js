c4920f95d070dd1937466621f9f8b22758eff026
Ali Zulfiqar <codewithfourtix@gmail.com>
2025-01-08 21:54:15 +0500

    finished

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const server = http.createServer((req, res) => {
   console.log("Request received");
   console.log(req.url);
   res.end("hello world from node js");
+  if (req.url == "/about") {
+    res.end("This is the about page");
+  }
+  if (req.url == "/contact") {
+    res.end("This is the contact page");
+  }
 });
 
 server.listen(3000);
