f96daaa11d34d5a9f9aee2586775db8d843e6a0e
Ali Zulfiqar <codewithfourtix@gmail.com>
2025-01-09 10:51:41 +0500

    morgan installation

--- express4.js
+++ express4.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.set("view engine", "ejs");
+
+app.use(morgan("dev"));
 
 app.use((req, res, next) => {
   console.log("Middleware executed");
